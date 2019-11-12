6ede6d7bb453e7455a742f871095d4bc7506d29d
Nick Hahn <nick.hahn@it-designers.de>
2019-11-12 09:02:57 +0100

    Add basic error handling

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ link=$1
 from=$(date -I --date="1 month ago")
 to=$(date -I --date="4 months")
 
-java -jar rapla2csv.jar -l "${1}" -f "${from}" -t "${to}" -o rapla.csv
+java -jar rapla2csv.jar -l "${1}" -f "${from}" -u "${to}" -o rapla.csv || exit 1
 
-python3 csv2ics.py -i rapla.csv -o dhbwcal.ics
+python3 csv2ical.py -i rapla.csv -o dhbwcal.ics || exit 1
 
 chmod 664 dhbwcal.ics 
 
-mv dhbwcal.ics /var/www/icalserver/
+mv dhbwcal.ics /var/www/icalserver/ || exit 1
 rm rapla.csv
