35ddb98274b3b72abd90b2823c61ed320719d6e8
Nick Hahn <nick.hahn@posteo.de>
2020-08-22 16:40:41 +0200

    Make course an argument so multiple jobs can run without overwriting files

--- run.sh
+++ run.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 link=$1
+course=$2
 from=$(date -I --date="1 month ago")
 to=$(date -I --date="4 months")
+dir='/var/dhbw-ical'
 
 cleanup() {
 
 	echo 'Error: ${1}'
-	rm rapla.csv || true
-	rm dhbwcal.ics || true
+	rm ${course}rapla.csv || true
+	rm ${course}dhbwcal.ics || true
 	exit 1
 
 }
-java -jar rapla2csv.jar -l "${1}" -f "${from}" -u "${to}" -o rapla.csv || cleanup 'Error fetching calendar'
+java -jar ${dir}/rapla2csv.jar -l "${1}" -f "${from}" -u "${to}" -o ${dir}/${course}rapla.csv || cleanup 'Error fetching calendar'
 
-python3 csv2ical.py -i rapla.csv -o dhbwcal.ics || cleanup 'Error converting csv to ical'
+python3 ${dir}/csv2ical.py -i ${dir}/${course}rapla.csv -o ${dir}/${course}dhbwcal.ics || cleanup 'Error converting csv to ical'
 
-chmod 664 dhbwcal.ics 
+mkdir -p ${dir}/${course}
 
-mv dhbwcal.ics /var/www/icalserver/ || cleanup 'Error moving ical to destination'
-rm rapla.csv
+chmod 664 ${dir}/${course}dhbwcal.ics 
 
+mv ${dir}/${course}dhbwcal.ics /var/www/dhbw-ical/${course}/dhbwcal.ics || cleanup 'Error moving ical to destination'
+rm ${dir}/${course}rapla.csv
