d07f3ccdfac8134532aa9fe9eba6286c05046d8d
Nick Hahn <nick.hahn@posteo.de>
2020-08-22 16:53:17 +0200

    Fix bad quoting

--- run.sh
+++ run.sh
@@ -8,13 +8,13 @@ dir='/var/dhbw-ical'
 
 cleanup() {
 
-	echo 'Error: ${1}'
+	echo "Error: ${1}"
 	rm ${course}rapla.csv || true
 	rm ${course}dhbwcal.ics || true
 	exit 1
 
 }
-java -jar ${dir}/rapla2csv.jar -l "${1}" -f "${from}" -u "${to}" -o ${dir}/${course}rapla.csv || cleanup 'Error fetching calendar'
+python3 ${dir}/rapla2csv/main.py "${1}" "${from}" "${to}" -o ${dir}/${course}rapla.csv || cleanup 'Error fetching calendar'
 
 python3 ${dir}/csv2ical.py -i ${dir}/${course}rapla.csv -o ${dir}/${course}dhbwcal.ics || cleanup 'Error converting csv to ical'
 
